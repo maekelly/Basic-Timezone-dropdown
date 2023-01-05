3923da5b7f12f87626cb41ba3ada9b945ad73d29
maekelly <112394441+maekelly@users.noreply.github.com>
2023-01-05 12:07:27 +0000

    Refactored code and streamlined JS
    Moved all the JS into the html file

--- src/app.js
+++ src/app.js
@@ -1,21 +1,10 @@
 function popupTime() {
-  if (event.target.value === "philippines") {
-    let phTime = moment()
-      .tz(`Asia/Manila`)
+  if (event.target.value.length > 0) {
+    let zoneSelected = event.target.value;
+    let zoneTime = moment()
+      .tz(`${zoneSelected}`)
       .format(`dddd Do MMMM YYYY, hh:mm A`);
-    alert(`It is ${phTime} in The Philippines`);
-  }
-  if (event.target.value === "scotland") {
-    let scTime = moment()
-      .tz(`Europe/London`)
-      .format(`dddd Do MMMM YYYY, hh:mm A`);
-    alert(`It is ${scTime} in Scotland`);
-  }
-  if (event.target.value === "uae") {
-    let uaeTime = moment()
-      .tz(`Asia/Dubai`)
-      .format(`dddd Do MMMM YYYY, hh:mm A`);
-    alert(`It is ${uaeTime} in The UAE`);
+    alert(`In the timezone '${zoneSelected}', it is ${zoneTime}`);
   }
 }
 
